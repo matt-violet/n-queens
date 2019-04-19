9e6292314060d0f4016ff79a0617a03c2244e4de
matt-violet <mattviolet@gmail.com>
2019-04-18 20:17:12 -0700

    implement solutions for NRooksSolution and NRooksSolution Count; Nearly complete solutions for NQueenSolution functions. Need to update backtracking.

--- src/solvers.js
+++ src/solvers.js
@@ -15,79 +15,129 @@
 
 
 
+
 //returns a single solution to the n-rooks problem
 window.findNRooksSolution = function (n) {
-  var solution = undefined; //fixme
+  var newBoard = new Board({ n: n });
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  var findSolution = function (board) {
+    var solution; //entire matrix, array of arrays
+    var count = 0;
+
+    for (var row = 0; row < n; row++) {
+      for (var col = 0; col < n; col++) {
+        board.togglePiece(row, col);
+        if (board.hasRowConflictAt(row) || board.hasColConflictAt(col)) {
+          board.togglePiece(row, col);
+        } else {
+          count++;
+        }
+      }
+    }
+    if (count === n) {
+      solution = board.rows();
+      return solution;
+    }
+  }
+
+  return findSolution(newBoard);
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 //returns a count of the total number of solutions to the n-rooks problem
 window.countNRooksSolutions = function (n) {
-  var solutionCount = undefined; //fixme
+  var newBoard = new Board({ n: n });
+  var solutionCount = 0;
 
+  var search = function (row) {
+    //base case
+    if (row === n - 1) {
+      solutionCount++;
+      return;
+    }
+
+    for (var col = 0; col < n; col++) {
+      newBoard.togglePiece(row, col);
+      if (!newBoard.hasAnyRooksConflicts()) {
+        search(row + 1);
+      }
+      newBoard.togglePiece(row, col);
+    }
+  };
+
+  search(0);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
-window.findNQueensSolution = function (n) {
-  var solution = undefined; //fixme
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
+
+window.findNQueensSolution = function (n) {
+  var newBoard = new Board({ n: n });
+  var solution;
+
+  var helper = function (row) {
+    //base case
+    debugger;
+    var found = false;
+    if (row === n) {
+      solution = newBoard.rows();
+      found = true;
+      return;
+    }
+    for (var col = 0; col < n; col++) {
+      newBoard.togglePiece(row, col);
+      if (newBoard.hasAnyQueensConflicts()) {
+        newBoard.togglePiece(row, col);
+        if (col === n - 1) {
+          helper(row + 1);
+        }
+      } else {
+        helper(row + 1);
+      }
+      if (found) {
+        return;
+      }
+
+      newBoard.togglePiece(row, col);
+    }
+  }
+
+  helper(0);
   return solution;
+  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 //returns a single solution to the n-queens problem
 window.countNQueensSolutions = function (n) {
-  var solutionCount = undefined; //fixme
+  var newBoard = new Board({ n: n });
+  var solutionCount = 0; //fixme
 
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
+  if (n === 2 || n === 3) {
+    return solutionCount;
+  }
+
+  var search = function (row) {
+    //base case
+    if (row === n) {
+      solutionCount++;
+      return;
+    }
+
+    for (var col = 0; col < n; col++) {
+      newBoard.togglePiece(row, col);
+      if (!newBoard.hasAnyQueensConflicts()) {
+        search(row + 1);
+      }
+      newBoard.togglePiece(row, col);
+    }
+  };
+
+  search(0);
+  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
 
-//dont need to create a matrix of the nxn table-this is how you would
-// var array = [];
-// for (var i = 0; i <= n; i++) {
-//   array[i] = [];
-//   for (var j = 0; j <= n; j++) {
-//     array[i].push(0);
-//   }
-// }
-
-
-
-//nRooks
-//can move left right, up or down
-//one rook per row (an column)
-
-//have an instance of the Board class
-  //the board class has attributes and current attributes
-
-
-//var occupiedRows = []; //containing row numbers (array index)
-//var firstRowPositions tried = []; array[0][i] i = 0 through (n-1)
-
-
-//Start with the left most column
-  //if no rooks are placed, place one in the first position (keep track of these in an array)
-//if all rooks are placed return true
-  //once you place a rook
-    //set the array element from 0 to 1
-    //
-
-//nQueens
-//Queens can move in any direction (left, right, up down, diagonally)
-//one Queen per row (and column)
-
-//have an instance of the Board class
-  //the board class has attributes and current attributes
-
-
-//Start with the left most column
-  //if no queens are placed, place one in the first position (keep track of these in an array)
-//if all queens are placed return true
